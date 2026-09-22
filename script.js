131e1556b53b450ae85d9182593a437105d9ee92
acuuiq <lord07821322@gmail.com>
2026-09-22 06:28:27 +0300

    إضافة خلفية متغيرة في الصفحة الرئيسية

--- script.js
+++ script.js
@@ -21,4 +21,15 @@ if (filterButtons.length > 0 && cards.length > 0) {
       });
     });
   });
+  // ===== سلايدر الخلفية =====
+  const heroSlides = document.querySelectorAll(".hero-slide");
+  let currentSlide = 0;
+
+  if (heroSlides.length > 0) {
+    setInterval(() => {
+      heroSlides[currentSlide].classList.remove("active");
+      currentSlide = (currentSlide + 1) % heroSlides.length;
+      heroSlides[currentSlide].classList.add("active");
+    }, 4000); // كل 4 ثوانٍ
+  }
 }
